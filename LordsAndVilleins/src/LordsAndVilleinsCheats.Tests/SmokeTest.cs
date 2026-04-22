ab32e3b3c4b958a06dc70e14714bd4699be17511
keven <taoca.ai@gmail.com>
2026-04-22 22:19:56 +0800

    test: scaffold xUnit project with smoke test

---
+++ LordsAndVilleins/src/LordsAndVilleinsCheats.Tests/SmokeTest.cs
@@ -0,0 +1,10 @@
+using Xunit;
+
+namespace LordsAndVilleinsCheats.Tests
+{
+    public class SmokeTest
+    {
+        [Fact]
+        public void TestInfrastructure_Works() => Assert.Equal(2, 1 + 1);
+    }
+}
